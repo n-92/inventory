081e25f5b81fd68a5807ee67b0de2e654fa678b0
Aung Naing Oo <aung0086@e.ntu.edu.sg>
2017-01-30 18:52:12 +0800

    Added the index page and item_detail page

--- inventory/settings.py
+++ inventory/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'inventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['inventory/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- inventory_tracker/views.py
+++ inventory_tracker/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from inventory_tracker.models import Item
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<p> In Index View</p>')
+    items = Item.objects.exclude(amount=0)
+    return render(request, 'inventory_tracker/index.html',{
+        'items': items,
+    })
 
 def item_detail(request, id):
-    return HttpResponse('<p> In item_detail view with id {0}'.format(id))
+    try:
+        item = Item.objects.get(id=id)
+    except Item.DoesNotExist:
+        raise Http404('This item does not exist')
+    return render(request, 'inventory_tracker/item_detail.html',{
+        'item':item,
+    })
